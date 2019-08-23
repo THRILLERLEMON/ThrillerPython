f7fc16eda93deaa1a4bffe71076cad4493c2eeb6
THRILLER <thrillerlemon@gmail.com>
2019-08-23 20:35:32 +0800

    change the shanxi path

--- Excel_Pandas/YR_AddBefore2000.py
+++ Excel_Pandas/YR_AddBefore2000.py
@@ -332,11 +332,11 @@ parsUni = {
 
 
 # shanxi
-path = 'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_3RD\\Data_Shanxi'
+path = 'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_2ED\\Data_Shanxi'
 allCountryData = pd.read_excel(
-    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_3RD\\YR_All_3RD_Test0823.xlsx', sheet_name="Sheet1")
+    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_2ED\\YR_All_2ED.xlsx', sheet_name="Sheet1")
 onetoN_Code = pd.read_excel(
-    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_3RD\\OnetoN_Code_3RD_Test0823.xlsx', sheet_name="Code")
+    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_2ED\\OnetoN_Code_2ED.xlsx', sheet_name="Code")
 
 
 findresult = allCountryData[allCountryData["Province_name"] == '山西省']
@@ -457,5 +457,5 @@ for tIndex, tRow in findresult.iterrows():
     print('##################################################')
     print('#endregion')
 allCountryData.to_csv(
-    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_3RD\\newCsv.csv', encoding='gbk')
+    'D:\\OneDrive\\SharedFile\\EXCEL 数据处理\\EXCELwork201908_linux_2ED\\AfterSX.csv', encoding='gbk')
 print('Already Finish Work! Good! THRILLER柠檬！')
