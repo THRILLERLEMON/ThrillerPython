691bac2d3b1c253dd4c7865c7fdefbbf492f4372
THRILLER <thrillerlemon@gmail.com>
2019-09-16 22:04:20 +0800

    add a script from MTEWork for apend new train data

---
+++ Excel_Pandas/MTE_JQL_addNewData.py
@@ -0,0 +1,21 @@
+# MTE work;add new train data to xlsx
+# Windows 10 1903
+# 2019.9.16
+# JiQiulei thrillerlemon@outlook.com
+
+import pandas as pd
+
+
+oldtable = pd.read_excel(
+    'E:\\OFFICE\\MTE_NEE_DATA\\GRA_Train_NEE_5.xlsx', sheet_name="All", encoding='gbk')
+
+gsocMon = pd.read_csv(
+    'E:\\OFFICE\\MTE_NEE_DATA\\addNewData.xlsx', sheet_name="GSOC")
+maMon = pd.read_csv(
+    'E:\\OFFICE\\MTE_NEE_DATA\\addNewData.xlsx', sheet_name="Manure_Application")
+nfaMon = pd.read_csv(
+    'E:\\OFFICE\\MTE_NEE_DATA\\addNewData.xlsx', sheet_name="Nfertilizer_Application")
+nhxMon = pd.read_csv(
+    'E:\\OFFICE\\MTE_NEE_DATA\\addNewData.xlsx', sheet_name="NHx_N_Deposition")
+noyMon = pd.read_csv(
+    'E:\\OFFICE\\MTE_NEE_DATA\\addNewData.xlsx', sheet_name="NOy_N_Deposition")
