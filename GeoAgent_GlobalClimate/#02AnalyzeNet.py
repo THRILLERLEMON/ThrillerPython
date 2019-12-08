abb394bea43ef4146f5f83a078814654b397ae2b
THRILLER <thrillerlemon@gmail.com>
2019-12-08 19:43:06 +0800

    change some var name to see more better

--- GeoAgent_GlobalClimate/#02AnalyzeNet.py
+++ GeoAgent_GlobalClimate/#02AnalyzeNet.py
@@ -37,7 +37,6 @@ def main():
     allNodes = pd.read_csv(geoAgentInfopath, header=0, dtype={'label': np.int32})
     # print(allLinks)
 
-    # draw2NetWorkOn3DMaps(NetG1,NetG2, geoAgentSHPpath, 'weight', 'tem_skew_mean')
     draw3DNetWork_2difVar(allNodes, allLinks, geoAgentSHPpath, 'Prs', 'Pre', 'Cij')
     # varList = {'Tem', 'Prs', 'Pre'}
     # for var in varList:
@@ -128,100 +127,79 @@ def draw2DNetWork_2Var(showOrSave, netWorkG, geoAgentShpPath, edgeColorF, nodeCo
         plt.savefig(outPutPath + mapTitle + ".png")
 
 
-def draw2NetWorkOn3DMaps(netWorkG1, netWorkG2, geoAgentShpPath, edgeColorF, nodeColorF):
-    fig = plt.figure(figsize=(10, 10), dpi=200)
-    ax = Axes3D(fig, xlim=[-180, 180], ylim=[-90, 90])
-    ax2 = Axes3D(fig, xlim=[-180, 180], ylim=[-90, 90])
-    ax.set_zlim(bottom=0)
-    ax2.set_zlim(bottom=0.2)
-    target_projection = ccrs.PlateCarree()
-    feature = ShapelyFeature(Reader(geoAgentShpPath).geometries(), ccrs.PlateCarree())
-    geoms = feature.geometries()
-    geoms = [target_projection.project_geometry(geom, feature.crs) for geom in geoms]
-    paths = list(itertools.chain.from_iterable(geos_to_path(geom) for geom in geoms))
-    segments = []
-    for path in paths:
-        vertices = [vertex for vertex, _ in path.iter_segments()]
-        vertices = np.asarray(vertices)
-        segments.append(vertices)
-
-    lc = LineCollection(segments, color='black', linewidths=0.1)
-    ax.add_collection3d(lc, 0)
-
-    pos = {}
-    for n, d in netWorkG1.nodes(data=True):
-        # transform lon & lat
-        mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
-        pos[n] = (mx, my)
-
-    Netedges1 = nx.draw_networkx_edges(netWorkG1, pos, width=0.15, alpha=0.7, arrows=False, edge_color='b')
-    Netedges2 = nx.draw_networkx_edges(netWorkG2, pos, width=0.15, alpha=0.7, arrows=False, edge_color='r')
-    ax.add_collection3d(Netedges1, 0)
-    ax.add_collection3d(Netedges2, 0.2)
-    # first draw point's edge in black to set Point edge
-    # nx.draw_networkx_nodes(netWorkG, pos, ax=ax, node_size=[netWorkG.degree(n) * 1.4 for n in netWorkG],
-    #                        node_color='k')
-    Netnodes = nx.draw_networkx_nodes(netWorkG1, pos, ax=ax2, node_size=[netWorkG1.degree(n) * 1.3 for n in netWorkG1],
-                                      node_color=[d[nodeColorF] for n, d in netWorkG1.nodes(data=True)],
-                                      cmap=plt.cm.OrRd)
-
-    # ax.plot(Netnodes,0.4)
-
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    plt.get_current_fig_manager().window.showMaximized()
-    plt.show()
-
-
 def draw3DNetWork_2difVar(allNodesData, allLinksData, geoAgentShpPath, SouVar, TarVar, weightQuota):
+    souBGC = '#F3B600'
+    souGAC = '#704300'
+    souNodeC = '#FF7335'
+    souEdgeC = '#FD7E00'
+    tarBGC = '#004FCC'
+    tarGAC = '#005562'
+    tarNodeC = '#1927FF'
+    tarEdgeC = '#2600F1'
+    twoLayerEdgeC = '#26F100'
+    twoLayerdis = 0.5
     fig = plt.figure(figsize=(10, 10), dpi=200)
     ax = Axes3D(fig, xlim=[-180, 180], ylim=[-90, 90])
-    ax.set_zlim(bottom=0)
+    ax.grid(False)
 
-    # drwa background
-    soup = Rectangle((-180, -90), 360, 180, color='#EA8512', ec='#FE8800', alpha=0.5)
-    ax.add_patch(soup)
-    art3d.pathpatch_2d_to_3d(soup, z=0, zdir="z")
-    ax.text(-180, -90, 0, SouVar, color='k')
-    tarp = Rectangle((-180, -90), 360, 180, color='#2D43CC', ec='#0122F9', alpha=0.5)
-    ax.add_patch(tarp)
-    art3d.pathpatch_2d_to_3d(tarp, z=0.5, zdir="z")
-    ax.text(-180, -90, 0.5, TarVar, color='k')
+    ### drwa background
+    souBG = Rectangle((-180, -90), 360, 180, color=souBGC, ec=souGAC, alpha=0.3)
+    ax.add_patch(souBG)
+    art3d.pathpatch_2d_to_3d(souBG, z=0, zdir="z")
+    ax.text(170, 90, 0, SouVar, color='k')
+    tarBG = Rectangle((-180, -90), 360, 180, color=tarBGC, ec=tarGAC, alpha=0.3)
+    ax.add_patch(tarBG)
+    art3d.pathpatch_2d_to_3d(tarBG, z=twoLayerdis, zdir="z")
+    ax.text(170, 90, 0.5, TarVar, color='k')
+    # add ageAgent
     target_projection = ccrs.PlateCarree()
-    feature = ShapelyFeature(Reader(geoAgentShpPath).geometries(), ccrs.PlateCarree())
-    geoms = feature.geometries()
-    geoms = [target_projection.project_geometry(geom, feature.crs) for geom in geoms]
-    paths = list(itertools.chain.from_iterable(geos_to_path(geom) for geom in geoms))
-    segments = []
-    for path in paths:
+    feaGeoAgent = ShapelyFeature(Reader(geoAgentShpPath).geometries(), ccrs.PlateCarree())
+    gemsGA = feaGeoAgent.geometries()
+    gemsGA = [target_projection.project_geometry(geom, feaGeoAgent.crs) for geom in gemsGA]
+    pathsGA = list(itertools.chain.from_iterable(geos_to_path(geom) for geom in gemsGA))
+    segsGA = []
+    for path in pathsGA:
         vertices = [vertex for vertex, _ in path.iter_segments()]
         vertices = np.asarray(vertices)
-        segments.append(vertices)
-    lc = LineCollection(segments, color='#FE8800', linewidths=0.1)
-    lc2 = LineCollection(segments, color='#0122F9', linewidths=0.1)
+        segsGA.append(vertices)
+    lc = LineCollection(segsGA, color=souGAC, linewidths=0.1, alpha=0.6)
     ax.add_collection3d(lc, 0)
-    ax.add_collection3d(lc2, 0.5)
+    lc2 = LineCollection(segsGA, color=tarGAC, linewidths=0.1, alpha=0.6)
+    ax.add_collection3d(lc2, twoLayerdis)
 
-    # draw Sou net
+    ### draw Sou net
     NetGS = buildNetWork_2Var(
         allNodesData=allNodesData,
         allLinksData=allLinksData,
         VarSou=SouVar,
         VarTar=SouVar,
         weightQuota=weightQuota)
-    posS = {}
+    posSou = {}
     for n, d in NetGS.nodes(data=True):
         # transform lon & lat
         mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
-        posS[n] = (mx, my)
-    NetedgesS = nx.draw_networkx_edges(NetGS, posS, width=0.5, alpha=0.7, arrows=False, edge_color='#F95301')
+        posSou[n] = (mx, my)
+    NetedgesS = nx.draw_networkx_edges(NetGS, posSou, width=0.5, alpha=0.7, arrows=False, edge_color=souEdgeC)
     ax.add_collection3d(NetedgesS, 0)
-    # first draw point's edge in black to set Point edge
-    # nx.draw_networkx_nodes(netWorkG, pos, ax=ax, node_size=[netWorkG.degree(n) * 1.4 for n in netWorkG],
-    #                        node_color='k')
-    nx.draw_networkx_nodes(NetGS, posS, node_size=[NetGS.degree(n) * 0.5 for n in NetGS],
-                           node_color='#F95301')
+    # Use this ax's ticks will disappear
+    # nx.draw_networkx_nodes(NetGS, posS, node_size=[NetGS.degree(n) * 0.5 for n in NetGS],
+    #                        node_color=souDarkC)
+    posSou = {}
+    Sdegeq0 = []
+    for an in NetGS:
+        if NetGS.degree(an) == 0:
+            Sdegeq0.append(an)
+    NetGS.remove_nodes_from(Sdegeq0)
+    for n, d in NetGS.nodes(data=True):
+        # transform lon & lat
+        mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
+        posSou[n] = (mx, my, 0, NetGS.degree(n))
+
+    for key, value in posSou.items():
+        xi = value[0]
+        yi = value[1]
+        zi = value[2]
+        ax.scatter(xi, yi, zi, alpha=0.8, c=souNodeC, s=value[3] * 0.5)
 
     ### draw Tar net
     NetGT = buildNetWork_2Var(
@@ -230,14 +208,14 @@ def draw3DNetWork_2difVar(allNodesData, allLinksData, geoAgentShpPath, SouVar, T
         VarSou=TarVar,
         VarTar=TarVar,
         weightQuota=weightQuota)
-    posTall = {}
+    posTar = {}
     for n, d in NetGT.nodes(data=True):
         # transform lon & lat
         mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
-        posTall[n] = (mx, my)
-    NetedgesT = nx.draw_networkx_edges(NetGT, posTall, width=0.5, alpha=0.7, arrows=False, edge_color='#0122F9')
-    ax.add_collection3d(NetedgesT, 0.5)
-    posT = {}
+        posTar[n] = (mx, my)
+    NetedgesT = nx.draw_networkx_edges(NetGT, posTar, width=0.5, alpha=0.7, arrows=False, edge_color=tarEdgeC)
+    ax.add_collection3d(NetedgesT, twoLayerdis)
+    posTar = {}
     Tdegeq0 = []
     for an in NetGT:
         if NetGT.degree(an) == 0:
@@ -246,15 +224,15 @@ def draw3DNetWork_2difVar(allNodesData, allLinksData, geoAgentShpPath, SouVar, T
     for n, d in NetGT.nodes(data=True):
         # transform lon & lat
         mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
-        posT[n] = (mx, my, 0.5, NetGT.degree(n))
+        posTar[n] = (mx, my, twoLayerdis, NetGT.degree(n))
 
-    for key, value in posT.items():
+    for key, value in posTar.items():
         xi = value[0]
         yi = value[1]
         zi = value[2]
-        ax.scatter(xi, yi, zi, alpha=0.8, c='#0122F9', linewidths=value[3] * 0.5)
+        ax.scatter(xi, yi, zi, alpha=0.8, c=tarNodeC, s=value[3] * 0.5)
 
-    ### draw ST net
+    ### draw Sou to Tar net
     NetGST = buildNetWork_2Var(
         allNodesData=allNodesData,
         allLinksData=allLinksData,
@@ -271,35 +249,56 @@ def draw3DNetWork_2difVar(allNodesData, allLinksData, geoAgentShpPath, SouVar, T
     for n, d in NetGST.nodes(data=True):
         # transform lon & lat
         mx, my = target_projection.transform_point(d['longitude'], d['latitude'], ccrs.PlateCarree())
-        # pos[n] = (mx, my, 0.2)
         if NetGST.in_degree(n) == 0:
-            pos[n] = (mx, my, 0)
+            pos[n] = (mx, my, 0, NetGST.degree(n))
         else:
-            pos[n] = (mx, my, 0.5)
+            pos[n] = (mx, my, twoLayerdis, NetGST.degree(n))
     for key, value in pos.items():
         xi = value[0]
         yi = value[1]
         zi = value[2]
         if zi != 0:
-            ax.scatter(xi, yi, zi, alpha=0.8, c='#0122F9')
+            degreeTar = 0
+            try:
+                degreeTar = posTar[key][3]
+            except:
+                degreeTar = 0
+            finally:
+                ax.scatter3D(xi, yi, zi, alpha=0.8, c=tarNodeC, s=(value[3] + degreeTar) * 0.5)
         else:
-            ax.scatter(xi, yi, zi, alpha=0.8, c='#F95301')
+            degreeSou = 0
+            try:
+                degreeSou = posSou[key][3]
+            except:
+                degreeSou = 0
+            finally:
+                ax.scatter3D(xi, yi, zi, alpha=0.8, c=souNodeC, s=(value[3] + degreeSou) * 0.5)
 
     for u, v, d in NetGST.edges(data=True):
         if d['Source'] == d['Target']:
             x = np.array((pos[u][0], pos[v][0]))
             y = np.array((pos[u][1], pos[v][1]))
-            z = np.array((0, 0.5))
-            ax.plot(x, y, z, c='g', alpha=0.6, linewidth=0.5)
-            ax.scatter(pos[u][0], pos[u][1], 0, alpha=0.8, c='#F95301')
+            z = np.array((0, twoLayerdis))
+            ax.plot(x, y, z, c=twoLayerEdgeC, alpha=0.7, linewidth=0.5)
+            degreeSou = 0
+            try:
+                degreeSou = posSou[d['Source']][3]
+            except:
+                degreeSou = 0
+            finally:
+                ax.scatter3D(pos[u][0], pos[u][1], 0, alpha=0.8, c=souNodeC, s=(pos[d['Source']][3] + degreeSou) * 0.5)
         x = np.array((pos[u][0], pos[v][0]))
         y = np.array((pos[u][1], pos[v][1]))
         z = np.array((pos[u][2], pos[v][2]))
-        ax.plot(x, y, z, c='g', alpha=0.6, linewidth=0.5)
+        ax.plot(x, y, z, c=twoLayerEdgeC, alpha=0.7, linewidth=0.5)
 
+    ### show fig
     ax.set_xlabel('longitude')
     ax.set_ylabel('latitude')
-    ax.set_zlabel('NetWorkLayer')
+    ax.set_zlabel('NetWork Layer')
+    ax.get_zaxis().set_ticks([])
+    mapTitle = 'NetWork ' + SouVar + ' to ' + TarVar + ' By THRILLER'
+    ax.set_title(mapTitle)
     plt.get_current_fig_manager().window.showMaximized()
     plt.show()
 
