9bfd76925f31ee0ecdd7034eedc2e3fc0bbc8463
THRILLER <thrillerlemon@gmail.com>
2020-01-08 17:17:35 +0800

    build a folder of GEE_LUCC work

---
+++ GEE_LUCC/AnaLocal_ComKindsAreafromCSVs.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/8 17:16
+# @Author  : THRILLER柠檬
+# @Email   : thrillerlemon@outlook.com
+# @File    : AnaLocal_ComKindsAreafromCSVs.py
+# @Software: PyCharm
